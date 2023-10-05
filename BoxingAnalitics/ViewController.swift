dbb233cb07dc64b7b6f9135dee5156662100c64f
woniderhaf <woniderhaf56@mail.ru>
2023-10-05 16:00:11 +0300

    Initial Commit

---
+++ BoxingAnalitics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoxingAnalitics
+//
+//  Created by Ivan Ivanov on 05.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
